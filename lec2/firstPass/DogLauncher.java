8deb5ac503829b30eb80c6e04b0420fa0ce964cf
JoshHug <hug@cs.berkeley.edu>
2015-01-22 18:25:01 -0800

    basic dog functionality

---
+++ lec2/firstPass/DogLauncher.java
@@ -0,0 +1,8 @@
+/* In HFJ (our textbook), we often create a 
+ * "test drive" class like this one to try out
+ * classes we've defined. */
+public class DogLauncher {
+    public static void main(String[] args) {
+        Dog.makeNoise();
+    }
+}
