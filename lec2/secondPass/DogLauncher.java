d5e51b5dbffd9251868787d73f51bc574bb15d06
JoshHug <hug@cs.berkeley.edu>
2015-01-22 21:53:48 -0800

    saved my first version of dog class

---
+++ lec2/secondPass/DogLauncher.java
@@ -0,0 +1,8 @@
+/* In HFJ, it is common to define a Test Drive
+ * class that will take our new class out for
+ * a spin. */
+public class DogLauncher {
+    public static void main(String[] args) {
+        Dog.makeNoise();
+    }
+} 
