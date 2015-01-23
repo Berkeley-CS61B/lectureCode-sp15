c03d592d9f905e9424d0a1f548f9365cc2756661
JoshHug <hug@cs.berkeley.edu>
2015-01-23 11:19:44 -0800

    I created an initial of dog

--- lec2/live1/Dog.java
+++ lec2/live1/Dog.java
@@ -1,5 +1,5 @@
 public class Dog {
     public static void makeNoise() {
         System.out.println("Bark!");
-    }
+    } 
 }
--- lec2/live1/DogLauncher.java
+++ lec2/live1/DogLauncher.java
@@ -5,5 +5,5 @@
 public class DogLauncher {
     public static void main(String[] args) {
         Dog.makeNoise();
-    }
+    } 
 }
