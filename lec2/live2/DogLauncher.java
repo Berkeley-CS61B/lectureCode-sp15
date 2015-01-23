338b31d5d63cffe35dd370fcd97c4fd769b37257
JoshHug <hug@cs.berkeley.edu>
2015-01-23 14:18:18 -0800

    created a basic dog class

---
+++ lec2/live2/DogLauncher.java
@@ -0,0 +1,8 @@
+/* In Head First Java, we often write a class
+ * that test drives our classes of interest.
+ */
+public class DogLauncher {
+    public static void main(String[] args) {
+        Dog.makeNoise();
+    }
+}
