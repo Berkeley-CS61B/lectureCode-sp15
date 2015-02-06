2b1e04ff2532556a3232aa6fd6ee790c4752ab2b
JoshHug <hug@cs.berkeley.edu>
2015-02-06 11:10:13 -0800

    updated hard mode code to hide some more stuff

--- lec8/exercises/hardMode/AList.java
+++ lec8/exercises/hardMode/AList.java
@@ -15,16 +15,15 @@ public class AList {
     public int getBack() {
         return 0;        
     }
+    /** Gets the ith item in the list (0 is the front). */
+    public int get(int i) {
+        return 0;        
+    }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int deleteBack() {
         return 0;
-    }
-
-    /** Gets the ith item in the list (0 is the front). */
-    public int get(int i) {
-        return 0;        
     }
 
     /** Returns the number of items in the list. */
--- lec8/exercises/hardMode/AListTest.java
+++ lec8/exercises/hardMode/AListTest.java
@@ -13,55 +13,30 @@ public class AListTest {
     }
 
     @Test
-    public void testSize() {
+    public void testInsertAndSize() {
         AList L = new AList();
         L.insertBack(99);
         L.insertBack(99);
         assertEquals(2, L.size());
     }
 
+    /*
     @Test
     public void testInsertAndGet() {
-        AList L = new AList();
-        L.insertBack(85);
-        assertEquals(85, L.getBack());
+    }*/
 
-        L.insertBack(8);
-        assertEquals(8, L.getBack());
-    }
-
-
+    /*
     @Test
     public void testGet() {
-        AList L = new AList();
-        L.insertBack(99);
-
-        assertEquals(99, L.get(0));
-
-        L.insertBack(8);
-        assertEquals(8, L.get(1));
-        assertEquals(99, L.get(0));
-    }
-
-    @Test
-    public void testDelete() {
-        AList L = new AList();
-        L.insertBack(50);
-        L.insertBack(99);
-        
-        int deleted = L.deleteBack();
-        assertEquals(99, deleted);
-        assertEquals(1, L.size());
-    }
+    }*/
 
     /*@Test
+    public void testDelete() {
+    }*/
+
+    /** Tests insertion of a large number of items.*/
+    /*@Test
     public void testMegaInsert() {
-        int N = 100000;
-        AList L = new AList();
-        for (int i = 0; i < N; i += 1) {
-            L.insertBack(i);
-        }
-        assertEquals(N - 1, L.getBack());
     }*/
 
     public static void main(String[] args) {
